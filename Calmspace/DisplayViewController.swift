07810f6eba3fa4fed1a983d249f6f49d64420141
Maryna Isaienko <macprobp@gmail.com>
2024-01-17 14:36:59 -0800

    Made final changes

--- Calmspace/DisplayViewController.swift
+++ Calmspace/DisplayViewController.swift
@@ -13,39 +13,39 @@ class DisplayViewController: UIViewController, WKNavigationDelegate {
     var link: URL?
     
     @IBOutlet weak var webView: WKWebView!
-    
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     @IBAction func refresh(_ sender: Any) {
-        
         webView.reload()
     }
     
     @IBAction func stop(_ sender: Any) {
-        
         webView.stopLoading()
     }
     
     @IBAction func rewind(_ sender: Any) {
-        
         if webView.canGoBack {
-            webView.goBack() }
+            webView.goBack()
+        }
     }
     
     @IBAction func forward(_ sender: Any) {
-        
         if webView.canGoForward {
-            webView.goForward() }
-        
+            webView.goForward()
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("DisplayViewController - View Did Load")
-        print("DisplayViewController - link: \(String(describing: link))")
-        
+        setupWebView()
+        loadInitialURL()
+    }
+    
+    private func setupWebView() {
         webView?.navigationDelegate = self
+    }
+    
+    private func loadInitialURL() {
         if let url = link {
             print("Received URL: \(url)")
             let request = URLRequest(url: url)
@@ -53,29 +53,17 @@ class DisplayViewController: UIViewController, WKNavigationDelegate {
         } else {
             print("URL is nil")
         }
-        
-        let backButton = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(backPressed))
-        
-        backButton.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .normal)
-        
-                navigationItem.leftBarButtonItem = backButton
     }
     
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-            spinner.startAnimating()
-        }
-        
-        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            spinner.stopAnimating()
-        }
+        spinner.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        spinner.stopAnimating()
+    }
     
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
         print("Web view failed to load with error: \(error.localizedDescription)")
     }
-    
-    @objc func backPressed() {
-            // Handle the back navigation item press
-            // For example, navigate back to HomeViewController
-            navigationController?.popToRootViewController(animated: true)
-        }
 }
